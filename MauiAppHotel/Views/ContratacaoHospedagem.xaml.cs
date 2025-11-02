38414f66ec6c470b324f3c06fa02efcc329f36d6
Paulomed99 <pakamedeiros@gmail.com>
2025-11-02 10:12:31 -0300

    Criação das páginas MainPage e Sobre.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,16 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private async void SobreClicked(object sender, EventArgs e)
+    {
+  
+        await Shell.Current.GoToAsync("Sobre");
+    }
+
+}
